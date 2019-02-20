1cb0d6e2ccef5f8215fd2b242c74a84fbf971cc5
c1178396429 <2109200262@qq.com>
2019-02-20 18:00:43 +0800

    添加

---
+++ 笔记/小程序/星旅会/弹窗/pages/index/index.js
@@ -0,0 +1,13 @@
+// pages/index/index.js
+const app= getApp()
+Page({
+  data:{
+    banner: [
+      "../../image/xlh_pic6@2x.png",
+      "../../image/xlh_pic6@2x.png",
+      "../../image/xlh_pic6@2x.png"
+    ],
+    bannerIndex:1
+  }
+ 
+})
---
+++ 笔记/小程序/星旅会/限时抢购/index/index.js
@@ -0,0 +1,16 @@
+var app = getApp()
+Page({
+  data: {
+    navbar: ['美食抢购', '酒店抢购', '即将开抢'],
+    currentTab: 0,
+  },
+
+  // 导航切换监听
+  navbarTap: function (e) {
+    console.debug(e);
+    this.setData({
+      currentTab: e.currentTarget.dataset.idx
+    })
+  },
+
+}) 
